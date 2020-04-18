687a27a8a0b55cc4c50fbaeeafa17d436ddff07c
Umer Shaikh <umershaikh305@gmail.com>
2020-04-19 03:22:38 +0500

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,17 +4,16 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: REACT_APP_apiKey,
-    authDomain: REACT_APP_authDomain,
-    databaseURL: REACT_APP_databaseURL,
-    projectId: REACT_APP_projectId,
-    storageBucket: REACT_APP_storageBucket,
-    messagingSenderId: REACT_APP_apiKey_messagingSenderId,
-    appId: REACT_APP_appId,
-    measurementId: REACT_APP_measurementId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_apiKey_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 export default firebase;
