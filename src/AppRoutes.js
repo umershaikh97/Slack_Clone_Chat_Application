0bc3fcd41a4504a50e5f9dd9a1e16aff3a02ba90
Umer Shaikh <umershaikh305@gmail.com>
2020-04-19 17:53:29 +0500

    Implement redux for the project

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import firebase from './firebase';
 import App from './App';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import { setUser } from './actions';
+import Spinner from './components/Spinner';
 
-const AppRoutes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path={'/'} component={App} />
-                <Route exact path={'/login'} component={Login} />
-                <Route exact path={'/register'} component={Register} />
-                <Redirect to={'/'} />
-            </Switch>
-        </Router>
+const AppRoutes = ({ history, setUser, isLoading }) => {
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                setUser(user)
+                history.push('/');
+            }
+        })
+    }, [])
+
+    return (isLoading ? <Spinner />
+        :
+        <Switch>
+            <Route exact path={'/'} component={App} />
+            <Route exact path={'/login'} component={Login} />
+            <Route exact path={'/register'} component={Register} />
+            <Redirect to={'/'} />
+        </Switch>
     )
 }
 
-export default AppRoutes;
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.userReducer.currentUser,
+        isLoading: state.userReducer.isLoading,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setUser: (...args) => { dispatch(setUser(...args)) },
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AppRoutes));
--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+import rootReducer from './reducers';
 
-ReactDOM.render(<AppRoutes />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}>
+    <BrowserRouter>
+        <AppRoutes />
+    </BrowserRouter>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
